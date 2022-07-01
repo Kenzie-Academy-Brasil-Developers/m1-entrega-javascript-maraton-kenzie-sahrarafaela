771d5ecc4d80704d753819bb88a74f886ba3cc6a
sahrarafaela <sararafaela704@gmail.com>
2022-07-01 12:11:48 -0300

    atualizacao

--- script.js
+++ script.js
@@ -1,13 +1,15 @@
 
-    let names = [  'Daniel' ,'Rafael' , 'Manoel' ]
+    let names = [ 'Manoel', 'Daniel','Rafael'  ]
 
 function positions (arr){
     
 let procurando = arr.indexOf('Daniel')
+let proc
 if (procurando !== 0){
-    // como daniel tem o bônus, ele sempre vai ficar um a menos do anterior
+    // como daniel tem o bônus, ele sempre vai ficar um a menos com o anterior a ele
+    proc = arr [(procurando - 1)]
 arr [(procurando - 1)] = 'Daniel'
-arr [procurando]
+arr [procurando] = proc
 return `Este é o pódio 1º ${arr[0]}, 2º ${arr[1]}, 3º ${arr[2]} `
 } else {
    return 'Daniel é o vencedor'
